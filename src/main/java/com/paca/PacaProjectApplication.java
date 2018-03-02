41ffa61f3934685a3debd17cd9a3245104f548b7
saulcasti <saul_casti@hotmail.es>
2018-03-02 11:48:53 +0100

    Primer commit
    Proyecto creado con STS.

---
+++ src/main/java/com/paca/PacaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.paca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacaProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/paca/PacaProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PacaProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
