a7e7f4a7083d9cb3bd4af64b83353acffdf7bc5a
saulcasti <saul_casti@hotmail.es>
2018-03-14 12:36:46 +0100

    Cambio de css

--- src/main/java/com/paca/validators/SignUpFormValidator.java
+++ src/main/java/com/paca/validators/SignUpFormValidator.java
@@ -19,8 +19,34 @@ public class SignUpFormValidator implements Validator{
 
 	@Override
 	public void validate(Object target, Errors errors) { 
-	
+		User user = (User) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Error.empty");
 
+		if (user.getEmail().length() < 5 || user.getEmail().length() > 24) {
+			errors.rejectValue("email", "Error.signup.email.length");
+		}
+
+		if (usersService.getUserEmail(user.getEmail()) != null) {
+			errors.rejectValue("email", "Error.signup.email.duplicate");
+
+		}
+
+		if (user.getName().length() < 5 || user.getName().length() > 24) {
+			errors.rejectValue("name", "Error.signup.name.length");
+		}
+
+		if (user.getLastName().length() < 5 || user.getLastName().length() > 24) {
+			errors.rejectValue("lastName", "Error.signup.lastName.length");
+		}
+
+		if (user.getPassword().length() < 5 || user.getPassword().length() > 24) {
+			errors.rejectValue("password", "Error.signup.password.length");
+		}
+
+		if (!user.getPasswordConfirm().equals(user.getPassword())) { 
+			errors.rejectValue("passwordConfirm",
+					"Error.signup.passwordConfirm.coincidence");
+		}
 	}
 
 }
